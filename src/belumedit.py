e42e11c9cad95628e14e3a1c33a60f653ef9b47f
falahsutawindaya-coder <54474247+falahsutawindaya-coder@users.noreply.github.com>
2020-03-14 22:16:34 +0700

    bikin komen
    kfjaopfkoa

--- src/belumedit.py
+++ src/belumedit.py
@@ -4,29 +4,36 @@ from  tkinter import ttk
 class GUI:
     def __init__(self, root):
         root.title("Tubes Kalkulator")
-        self.root = root
-        self.list = []
+        # Constructor queue history, tombol answer, dan parsing expression
         self.history = []
         self.ans = None
         self.expression = ''
-        self.getButtonToScreen()
-        
-        self.screen = Text(root, state='disabled', width=30, height=2,background="white", foreground="black")
-        self.screen.grid(row=0,column=0,columnspan=4,padx=5,pady=5)
-        self.screen.configure(state='normal')
-   
-    def pushToScreen(self, exp_form, newline=False):
-        self.screen.configure(state='normal')
-        self.screen.insert(END, exp_form)
-        self.expression += str(exp_form)
-        self.screen.configure(state ='disabled')
+        self.root = root
 
-    def deleteScreen(self):
+        # Push Grid Button
+        self.list = []
+        self.getButtonToForm()
+        
+        # Form Ekspresi
+        self.form = Text(root, state='disabled', width=30, height=2,background="white", foreground="black")
+        self.form.grid(row=0,column=0,columnspan=4,padx=5,pady=5)
+        self.form.configure(state='normal')
+   
+    def pushToForm(self, exp_form, newline=False):
+        # kegunaan : memasukan karakter satu per satu
+        self.form.configure(state='normal')
+        self.form.insert(END, exp_form)
+        self.expression += str(exp_form)
+        self.form.configure(state ='disabled')
+
+    def deleteForm(self):
+        # kegunaan : delete semua karakter pada form
         self.expression = ''
-        self.screen.configure(state='normal')
-        self.screen.delete('1.0', END)
+        self.form.configure(state='normal')
+        self.form.delete('1.0', END)
     
-    def getButtonToScreen(self):    
+    def getButtonToForm(self):    
+        # kegunaan : membuat button kalkulator
         self.list.append(self.onClick(u"\u221A"))
         self.list.append(self.onClick('MC',None))
         self.list.append(self.onClick('MR',None))
@@ -51,11 +58,13 @@ class GUI:
         self.grid_bottom()
 
     def grid_bottom(self):
+        # kegunaan : mengatur grid
         equalButton = self.onClick('=',None,9).grid(row=6,column=3,columnspan=1)
         openButton = self.onClick(u"\u0028").grid(row=6,column=1,columnspan=1)
         closeButton = self.onClick(u"\u0029").grid(row=6,column=2,columnspan=1)
 
     def grid_up(self):
+        # kegunaan : mengatur grid
         index=0
         i=1
         while (i<6):
@@ -65,9 +74,11 @@ class GUI:
             i=i+1
 
     def onClick(self, _text, push=True,width=9):
+        # kegunaan : event-handler button dan memasukannya ke Form satu per satu
         return ttk.Button(self.root, text=_text, command = lambda: self.Parse(_text, push), width=9)
 
     def parseExpAkar(self, express):
+        # kegunaan : membuat ekspresi akar menjadi ekspresi python
         akar = u"\u221A"
         newExp = ''
         i = 0
@@ -85,8 +96,9 @@ class GUI:
         return newExp
 
     def Parse(self, getExpr, push=True):
+        # kegunaan : memproses parsing dari Form GUI
         if (push != None):
-            self.pushToScreen(getExpr)
+            self.pushToForm(getExpr)
         else:   
             if (getExpr == '='): 
                 self.expression = self.expression.replace(u"\u00F7", '/')
@@ -97,14 +109,14 @@ class GUI:
                 print(self.expression)
                 result = eval(self.expression)
                 self.ans = result
-                self.deleteScreen()
-                self.pushToScreen(result, newline=True)
+                self.deleteForm()
+                self.pushToForm(result, newline=True)
             elif getExpr == "clear":
-                self.deleteScreen()
+                self.deleteForm()
             elif getExpr == "MC":
                 self.history.append(self.ans)
             elif getExpr == "MR":
-                self.pushToScreen(self.history.pop(), newline=True)
+                self.pushToForm(self.history.pop(), newline=True)
 
 root = Tk()
 create = GUI(root)
