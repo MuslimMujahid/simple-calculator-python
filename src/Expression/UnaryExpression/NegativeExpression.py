bd130181c7ba47bd3678e9037546cba897945900
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-03-17 07:15:09 +0700

    edit negativeexpression

--- src/Expression/UnaryExpression/NegativeExpression.py
+++ src/Expression/UnaryExpression/NegativeExpression.py
@@ -1,8 +1,11 @@
 from .UnaryExpression import UnaryExpression
 
 class NegativeExpression(UnaryExpression):
+    '''
+    value : TerminalExpression
+    '''
     def __init__(self, value):
-        self.value = value
+        self.value = value.value()
     def solve(self):
         return -self.value
 
