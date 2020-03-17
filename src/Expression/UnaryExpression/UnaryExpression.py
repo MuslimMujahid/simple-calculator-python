037e925e78ae520d47998be2b3a92b262357e373
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-03-17 07:13:22 +0700

    edit unaryexpression

--- src/Expression/UnaryExpression/UnaryExpression.py
+++ src/Expression/UnaryExpression/UnaryExpression.py
@@ -1,9 +1,10 @@
 from ..BaseExpression.Expression import Expression
 from abc import ABC, abstractmethod
 
-class UnaryExpression(ABC):
+class UnaryExpression(Expression):
     def __init__(self, value):
         self.value = float(value)
+        
     @abstractmethod
     def solve(self):
         pass
